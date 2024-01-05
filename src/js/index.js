16560f15dac6190adb6cfea8495b81588a71f776
Victor <victorg.lucindo@gmail.com>
2024-01-05 18:03:49 -0300

    primeira versao

---
+++ src/js/index.js
@@ -0,0 +1,15 @@
+const minhaImagem = document.querySelectorAll('.projetos');
+
+
+minhaImagem.forEach(minhaImagem =>{
+
+
+    
+
+minhaImagem.addEventListener("mouseover", () =>minhaImagem.classList.add("imagem-colorida"))
+  
+
+minhaImagem.addEventListener("mouseout", () =>  minhaImagem.classList.remove("imagem-colorida"));
+
+
+})
